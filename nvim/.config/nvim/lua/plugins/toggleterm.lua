8482e4470b60af4955a4fe4746f791d682209600
kyle <kyle@kylelee.dev>
2024-10-10 14:17:56 -0400

    add task warrior toggle term

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -26,6 +26,28 @@ return {
 			lazygit:toggle()
 		end
 
+		local task = Terminal:new({
+			cmd = "task list",
+			direction = "float",
+			float_opts = {
+				border = "double",
+			},
+			-- function to run on opening the terminal
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			-- function to run on closing the terminal
+			on_close = function(term)
+				vim.cmd("startinsert!")
+			end,
+		})
+
+		function _task_toggle()
+			task:toggle()
+		end
+
 		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>lua _task_toggle()<CR>", { noremap = true, silent = true })
 	end,
 }
