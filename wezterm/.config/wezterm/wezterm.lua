6fa82345e5b7011891c43de9d8282d792800a807
Kyle Lee <kyle@kylelee.dev>
2024-11-05 22:52:55 -0500

    remove weztemr keybindings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local font_config = require("fonts")
 local keybind_config = require("keybinds")
--- local utils = require("utils")
+local utils = require("utils")
 local ssh_domains = {
 	{
 		-- the name	to use for the 'wezterm connect' command
@@ -24,27 +24,27 @@ local config = {
 	automatically_reload_config = true,
 	-- enable_wayland = true,
 	font = font_config.get_fonts(),
-	font_size = 16.0,
+	font_size = 18.0,
 	-- color_scheme = "Dark+",
 	-- keys = keybind_config.get_keybinds(),
 	exit_behavior = "Hold",
 	default_prog = { "/bin/zsh", "-l" },
 }
 
--- local host_os = utils.detect_host_os()
--- local ghcup_PATH = "/Users/kyle/.ghcup/bin:"
--- local bin_PATH = "/usr/local/bin:" .. os.getenv("PATH")
--- local brew_PATH = "/opt/homebrew/bin/:"
--- local node_PATH = "/Users/kyle/.nvm/versions/node/v21.0.0/bin:"
--- local pnpm_PATH = "/Users/kyle/.nvm/versions/node/v21.0.0/bin:"
---
--- local PATH_ENV_VARS = ghcup_PATH .. bin_PATH .. brew_PATH .. node_PATH .. pnpm_PATH
--- if host_os == "macos" then
--- 	-- check homebrew binary symlinks on startup.
--- 	config.set_environment_variables = {
--- 		PATH = PATH_ENV_VARS,
--- 	}
--- end
+local host_os = utils.detect_host_os()
+local ghcup_PATH = "/Users/kyle/.ghcup/bin:"
+local bin_PATH = "/usr/local/bin:" .. os.getenv("PATH")
+local brew_PATH = "/opt/homebrew/bin/:"
+local node_PATH = "/Users/kyle/.nvm/versions/node/v21.0.0/bin:"
+local pnpm_PATH = "/Users/kyle/.nvm/versions/node/v21.0.0/bin:"
+
+local PATH_ENV_VARS = ghcup_PATH .. bin_PATH .. brew_PATH .. node_PATH .. pnpm_PATH
+if host_os == "macos" then
+	-- check homebrew binary symlinks on startup.
+	config.set_environment_variables = {
+		PATH = PATH_ENV_VARS,
+	}
+end
 -- wezterm.log_info(config)
 -- wezterm.log_error("Config Dir " .. wezterm.config_dir)
 return config
