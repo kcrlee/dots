6cb4d54becc81ca2a8d4730b1e7088aa11051e5e
Kyle Lee <kyle@kylelee.dev>
2024-12-01 00:37:25 -0500

    add neogit. remove toggleterm, fugitive. update lock

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -11,17 +11,13 @@ return {
 	config = function()
 		local markview = require("markview")
 		markview.setup({
-			hybrid_modes = {"n"},
+			hybrid_modes = { "n" },
 			callbacks = {
 				on_enable = function(_, win)
-					vim.wo[win].conceal = 2;
+					vim.wo[win].conceal = 2
 					vim.wo[win].concealcursor = "c"
-				end
-			}
-
-
-
+				end,
+			},
 		})
-
 	end,
 }
