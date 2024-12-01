8c92bbffd8f9d4ce338431602a77a92d54e5cc03
Kyle Lee <kyle@kylelee.dev>
2024-12-01 13:15:08 -0500

    add rest.

--- nvim/.config/nvim/lua/ftplugins/http.lua
+++ nvim/.config/nvim/lua/ftplugins/http.lua
@@ -5,3 +5,17 @@ vim.api.nvim_buf_set_keymap(
 	"<cmd>lua require('kulala').run()<cr>",
 	{ noremap = true, silent = true, desc = "Execute the request" }
 )
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	"<leader>co",
+	"<cmd>lua require('kulala').copy()<cr>",
+	{ noremap = true, silent = true, desc = "Copy the current request as a curl command" }
+)
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	"<leader>ci",
+	"<cmd>lua require('kulala').from_curl()<cr>",
+	{ noremap = true, silent = true, desc = "Paste curl from clipboard as http request" }
+)
