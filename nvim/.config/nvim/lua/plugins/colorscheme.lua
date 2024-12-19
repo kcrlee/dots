96e3930a9cd08cf7e5a74f55fbf371f389ee522e
Kyle Lee <kyle@kylelee.dev>
2024-12-19 02:27:02 -0500

    refactor to use vscode again until new colorscheme is ready

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 return {
-	dir = "~/dev/harbor",
-	dev = true,
+	"Mofiqul/vscode.nvim",
 	lazy = false,
 	config = function()
-		-- require("harbor").setup({})
-		vim.cmd("colorscheme harbor")
+		local code = require("vscode")
+		code.setup({
+			-- Enable transparent background
+			transparent = true,
+		})
+		vim.cmd.colorscheme("vscode")
 	end,
 }
+
+-- return {
+-- 	dir = "~/dev/harbor",
+-- 	dev = true,
+-- 	lazy = false,
+-- 	config = function()
+-- 		-- require("harbor").setup({})
+-- 		vim.cmd("colorscheme harbor")
+-- 	end,
+-- }
--- nvim/.config/nvim/lua/plugins/notify.lua
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -1,9 +1,12 @@
 return {
 	"rcarriga/nvim-notify",
-	opts = {
-		timeout = 1000,
-		render = "compact",
-		stages = "fade",
-		top_down = false,
-	},
+	config = function()
+		require("notify").setup({
+			timeout = 1000,
+			render = "compact",
+			stages = "fade",
+			top_down = false,
+			background_colour = "#000000",
+		})
+	end,
 }
