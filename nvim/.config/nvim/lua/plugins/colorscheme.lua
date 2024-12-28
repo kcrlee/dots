183b1a63f51178cf3ad1971d40fdabc9dac28238
Kyle Lee <kyle@kylelee.dev>
2024-12-28 01:22:52 -0500

    update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
-	{
-		"Mofiqul/vscode.nvim",
-		lazy = false,
-		config = function()
-			local code = require("vscode")
-			code.setup({
-				-- Enable transparent background
-				transparent = true,
-			})
-			vim.cmd.colorscheme("vscode")
-		end,
-	},
+	"Mofiqul/vscode.nvim",
+	lazy = false,
+	config = function()
+		local code = require("vscode")
+		code.setup({
+			-- Enable transparent background
+			transparent = true,
+		})
+		vim.cmd.colorscheme("vscode")
+	end,
 }
