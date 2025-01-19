728a21784f9fc17133f8e90fd32d45747032740b
Kyle Lee <kyle@kylelee.dev>
2025-01-19 00:43:38 -0500

    add render-markdown, typst-preview, markdown-preview

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,23 +1,23 @@
 return {
-	"OXY2DEV/markview.nvim",
-	lazy = false, -- Recommended
-	-- ft = "markdown" -- If you decide to lazy-load anyway
-
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
-
-	config = function()
-		local markview = require("markview")
-		markview.setup({
-			hybrid_modes = { "n" },
-			callbacks = {
-				on_enable = function(_, win)
-					vim.wo[win].conceal = 2
-					vim.wo[win].concealcursor = "c"
-				end,
-			},
-		})
-	end,
+	-- "OXY2DEV/markview.nvim",
+	-- lazy = false, -- Recommended
+	-- -- ft = "markdown" -- If you decide to lazy-load anyway
+	--
+	-- dependencies = {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- },
+	--
+	-- config = function()
+	-- 	local markview = require("markview")
+	-- 	markview.setup({
+	-- 		hybrid_modes = { "n" },
+	-- 		callbacks = {
+	-- 			on_enable = function(_, win)
+	-- 				vim.wo[win].conceal = 2
+	-- 				vim.wo[win].concealcursor = "c"
+	-- 			end,
+	-- 		},
+	-- 	})
+	-- end,
 }
