01ecd0517a8606009ffe4c4ccb0fa60bf923e2e1
Kyle Lee <kyle@kylelee.dev>
2025-07-03 01:00:02 -0400

    swap cspell for typos_lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -118,12 +118,20 @@ return {
 		lspconfig.bashls.setup({
 			capabilities = capabilities,
 		})
-
-		lspconfig.cspell_ls.setup({
-			capabilities = capabilities,
-			filetypes = { "go", "rust", "js", "ts", "html", "css", "json", "yaml", "markdown", "gitcommit" },
-			root_dir = require("lspconfig.util").root_pattern(".git", "package.json"),
+		lspconfig.typos_lsp.setup({
+			-- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+			cmd_env = { RUST_LOG = "error" },
+			init_options = {
+				-- Custom config. Used together with a config file found in the workspace or its parents,
+				-- taking precedence for settings declared in both.
+				-- Equivalent to the typos `--config` cli argument.
+				config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
+				-- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+				-- Defaults to error.
+				diagnosticSeverity = "Warning",
+			},
 		})
+
 		require("fidget").setup({})
 		require("mason").setup()
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
