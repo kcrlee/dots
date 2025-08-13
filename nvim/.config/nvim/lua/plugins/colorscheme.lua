86059445c589bcfe92e0a15c503d6fa987f6c889
kyle <kyle@kylelee.dev>
2025-08-13 15:52:49 -0400

    revert

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
-return {
-	"Mofiqul/vscode.nvim",
-	config = function()
-		local code = require("vscode").setup({})
-		vim.o.background = "dark"
-		vim.cmd("colorscheme vscode")
-	end,
-}
 -- return {
--- 	dir = "~/dev/personal/nvim-plugins/tomorrow-min",
--- 	-- "KyleLee95/tomorrow-min",
--- 	lazy = false,
+-- 	"Mofiqul/vscode.nvim",
 -- 	config = function()
--- 		vim.cmd("colorscheme tomorrow-min")
+-- 		local code = require("vscode").setup({})
+-- 		vim.o.background = "dark"
+-- 		vim.cmd("colorscheme vscode")
 -- 	end,
 -- }
+return {
+	dir = "~/dev/personal/nvim-plugins/tomorrow-min",
+	-- "KyleLee95/tomorrow-min",
+	lazy = false,
+	config = function()
+		vim.cmd("colorscheme tomorrow-min")
+	end,
+}
 
 -- return {
 -- 	"armannikoyan/rusty",
