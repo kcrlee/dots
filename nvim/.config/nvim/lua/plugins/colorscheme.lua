8fdb0e8b1a92d05ba5a4f6d19b5cc957d84a1103
Kyle Lee <kyle@kylelee.dev>
2025-08-14 11:59:02 -0400

    remove dead code

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,3 @@
--- return {
--- 	"Mofiqul/vscode.nvim",
--- 	config = function()
--- 		local code = require("vscode").setup({})
--- 		vim.o.background = "dark"
--- 		vim.cmd("colorscheme vscode")
--- 	end,
--- }
 return {
 	dir = "~/dev/personal/nvim-plugins/tomorrow-min",
 	-- "KyleLee95/tomorrow-min",
@@ -14,18 +6,3 @@ return {
 		vim.cmd("colorscheme tomorrow-min")
 	end,
 }
-
--- return {
--- 	"armannikoyan/rusty",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {
--- 		transparent = true,
--- 		italic_comments = true,
--- 		underline_current_line = true,
--- 	},
--- 	config = function(_, opts)
--- 		require("rusty").setup(opts)
--- 		vim.cmd("colorscheme rusty")
--- 	end,
--- }
