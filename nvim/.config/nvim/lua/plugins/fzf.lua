8979ca65d02965b5cbffb5d57be7157481faef8a
Kyle Lee <kyle@kylelee.dev>
2025-08-16 00:30:12 -0400

    update fzflua to use live_grep without glob

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -23,7 +23,7 @@ return {
 		vim.api.nvim_set_keymap("n", "<leader>ff", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
 		vim.api.nvim_set_keymap("n", "<leader>ff", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
 		vim.api.nvim_set_keymap("n", "<leader>fb", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
-		vim.api.nvim_set_keymap("n", "<leader>fg", [[<Cmd>lua require"fzf-lua".live_grep_glob()<CR>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>fg", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
 		vim.api.nvim_set_keymap("n", "<leader>k", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
 		vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
 		vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
