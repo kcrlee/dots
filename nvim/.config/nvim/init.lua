dd833e5a87437ac3481a3ea5b1877d7cef17c4f2
Kyle Lee <kyle@kylelee.dev>
2025-09-27 02:02:07 -0400

    working tree sitter install

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 local utils = require("utils")
 local vim = vim
-vim.cmd([[set mouse=]])
 
+vim.cmd([[set mouse=]])
 -- Config
 vim.o.spelllang = "en_us"
 vim.o.undofile = true
@@ -14,6 +14,10 @@ vim.o.number = true
 vim.o.winborder = "rounded"
 vim.o.signcolumn = "yes"
 vim.o.cursorcolumn = false
+
+-- Splits
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Text layout
 vim.o.wrap = false
@@ -31,13 +35,13 @@ vim.opt.grepformat = "%f:%l:%c:%m"
 --global
 vim.g.mapleader = ","
 
-
-utils.add_plugin({
+vim.pack.add({
 	utils.gh("nvim-lua/plenary.nvim"),
+	utils.gh("nvim-treesitter/nvim-treesitter-context"),
+	utils.gh("github.com/neovim/nvim-lspconfig"),
 	utils.gh("nvim-tree/nvim-web-devicons"),
 	utils.gh("stevearc/oil.nvim"),
 	utils.gh("mason-org/mason.nvim"),
-	utils.gh("nvim-treesitter/nvim-treesitter"),
 	utils.gh("NeogitOrg/neogit"),
 	utils.gh("stevearc/oil.nvim"),
 	utils.gh("ibhagwan/fzf-lua"),
@@ -46,23 +50,109 @@ utils.add_plugin({
 	utils.gh("folke/lazydev.nvim"),
 	utils.gh("saghen/blink.nvim"),
 	utils.gh("folke/tokyonight.nvim"),
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 vim.cmd[[colorscheme tokyonight-storm]]
 
 
 local fzf = require("fzf-lua")
-local mason = require("mason").setup({})
-local ts = require("nvim-treesitter")
+fzf.setup({'fzf-native'})
+
+local mason = require("mason")
+mason.setup({})
+
+local oil = require('oil')
+oil.setup({})
 
 
+local neogit = require('neogit')
+neogit.setup({})
 
 local map = vim.keymap.set
+
 map("n", "<leader>g", require("neogit").open)
 map("n", "<F12>", ":UndotreeToggle <Enter>")
 map("n", "<leader>vc", ":VimtexCompile <Enter>")
 map("n", "<leader>i", ":Inspect <Enter>")
 map("n", "<leader>ff", fzf.files)
 map("n", "<leader>fg", fzf.live_grep)
-map("n", "-", "<CMD>Oil<CR>")
+map("n", "-", ":Oil<CR>")
 
+-- LSP
+--
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup("config", { clear = true })
+local function setup_lsp()
+	vim.lsp.enable({
+		"cssls", -- npm i -g vscode-langservers-extracted
+		"gopls", -- os package mgr: gopls
+		"html",
+		"jsonls",
+		"lua_ls", -- os package mgr: lua-language-server
+		"pyright", -- npm i -g pyright
+		"ts_ls", -- npm i -g typescript typescript-language-server
+		"html_lsp",
+		"tailwindcss"
+	})
+
+	autocmd("LspAttach", {
+		group = augroup,
+		callback = function(ev)
+			local bufopts = { noremap = true, silent = true, buffer = ev.buf }
+			map("n", "gd", vim.lsp.buf.definition, bufopts)
+			map("n", "K", vim.lsp.hover, bufopts)
+        	map("n", "M", vim.diagnostic.open_float, bufopts)
+
+			local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+			local methods = vim.lsp.protocol.Methods
+			if client:supports_method(methods.textDocument_completion) then
+				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			end
+		end,
+	})
+end
+
+
+local function setup_ts()
+    	local ts = require('nvim-treesitter')
+		require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"bash", "c", "dockerfile", "fish", "git_config", "git_rebase",
+			"gitattributes", "gitcommit", "gitignore", "go", "gomod", "gosum",
+			"html", "javascript", "json", "lua", "make", "markdown", "python",
+			"sql", "toml", "tsx", "typescript", "yaml", "zig",
+		},
+		highlight = { enable = true },
+		indent = { enable = true },
+	})
+	-- Get the module reference
+	
+	-- Fix: use augroup instead of undefined 'augroup'
+	autocmd("PackChanged", {
+		group = augroup, -- was: augroup (undefined)
+		callback = function(ev)
+			local spec = ev.data.spec
+			if spec and spec.name == "nvim-treesitter" and ev.data.kind == "update" then
+				vim.schedule(function()
+					ts.update()
+				end)
+			end
+		end,
+	})
+	
+	autocmd("FileType", {
+		group = augroup, -- was: augroup (undefined)
+		callback = function(ev)
+			local filetype = ev.match
+			local lang = vim.treesitter.language.get_lang(filetype)
+			if vim.treesitter.language.add(lang) then
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.treesitter.start()
+			end
+		end,
+	})
+end
+
+setup_ts()
+setup_lsp()
--- nvim/.config/nvim/utils.lua
+++ nvim/.config/nvim/utils.lua
@@ -1,22 +1,25 @@
-local function gh(url)
-	return "https://github.com/" .. url
+local utils = {}
+
+function utils.gh(url)
+    return "https://github.com/" .. url
 end
 
-local function add_plugin(plugins, opts)
-	opts = opts or {}
-	local function do_add(plugs)
-		vim.pack.add(plugs)
-		for _, plug in ipairs(plugs) do
-			if type(plug) == "string" then
-				plug = { src = plug }
-			end
-			local name = plug.name or plug.src
-			name = vim.fs.basename(name)
-			name = name:gsub("%.n?vim", "")
-			name = name:gsub("%.", "-")
-			pcall(require, "plugins." .. name)
-		end
-	end
-
-	do_add(plugins)
+function utils.add_plugin(plugins, opts)
+    opts = opts or {}
+    local function do_add(plugs)
+        vim.pack.add(plugs)
+        for _, plug in ipairs(plugs) do
+            if type(plug) == "string" then
+                plug = { src = plug }
+            end
+            local name = plug.name or plug.src
+            name = vim.fs.basename(name)
+            name = name:gsub("%.n?vim", "")
+            name = name:gsub("%.", "-")
+            pcall(require, "plugins." .. name)
+        end
+    end
+    do_add(plugins)
 end
+
+return utils
