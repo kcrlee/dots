2d892354b8631773f2da5593bb01e8bc8b989c68
Kyle Lee <kyle@kylelee.dev>
2025-09-27 21:23:07 -0400

    add LSPs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,25 +38,46 @@ vim.g.mapleader = ","
 
 vim.pack.add({
 	utils.gh("nvim-lua/plenary.nvim"),
+
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	utils.gh("nvim-treesitter/nvim-treesitter-context"),
+
+	utils.gh("neovim/nvim-lspconfig"),
+
 	utils.gh("nvim-tree/nvim-web-devicons"),
-	utils.gh("github.com/neovim/nvim-lspconfig"),
-	utils.gh("stevearc/oil.nvim"),
-	utils.gh("mason-org/mason.nvim"),
 	utils.gh("NeogitOrg/neogit"),
+
+	utils.gh("mason-org/mason.nvim"),
+	utils.gh("mbbill/undotree"),
+
 	utils.gh("stevearc/oil.nvim"),
+
 	utils.gh("ibhagwan/fzf-lua"),
+
 	utils.gh("folke/snacks.nvim"),
 	utils.gh("folke/trouble.nvim"),
 	utils.gh("folke/lazydev.nvim"),
-	-- utils.gh("saghen/blink.nvim"),
 	utils.gh("folke/tokyonight.nvim"),
-	utils.gh("stevearc/conform.nvim"),
-	utils.gh("mfussenegger/nvim-lint")
+
+	utils.gh("saghen/blink.nvim"),
+
+	-- utils.gh("stevearc/conform.nvim"),
+	-- utils.gh("mfussenegger/nvim-lint")
 })
 
 vim.cmd [[colorscheme tokyonight-storm]]
+
+
+local snacks = require('snacks')
+snacks.setup({
+	image = { enabled = true },
+	bigfile = { enabled = true },
+	explorer = { enabled = true },
+})
+
+local trouble = require('trouble')
+trouble.setup({})
+
 
 
 local fzf = require("fzf-lua")
@@ -66,42 +87,265 @@ local mason = require("mason")
 mason.setup({})
 
 local oil = require('oil')
-oil.setup({})
+oil.setup({
+	-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+	-- Set to false if you still want to use netrw.
+	default_file_explorer = true,
+	-- Id is automatically added at the beginning, and name at the end
+	-- See :help oil-columns
+	columns = {
+		"icon",
+		-- "permissions",
+		-- "size",
+		-- "mtime",
+	},
+	-- Buffer-local options to use for oil buffers
+	buf_options = {
+		buflisted = false,
+		bufhidden = "hide",
+	},
+	-- Window-local options to use for oil buffers
+	win_options = {
+		wrap = false,
+		signcolumn = "no",
+		cursorcolumn = false,
+		foldcolumn = "0",
+		spell = false,
+		list = false,
+		conceallevel = 3,
+		concealcursor = "nvic",
+	},
+	-- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+	delete_to_trash = true,
+	-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+	skip_confirm_for_simple_edits = false,
+	-- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+	-- (:help prompt_save_on_select_new_entry)
+	prompt_save_on_select_new_entry = true,
+	-- Oil will automatically delete hidden buffers after this delay
+	-- You can set the delay to false to disable cleanup entirely
+	-- Note that the cleanup process only starts when none of the oil buffers are currently displayed
+	cleanup_delay_ms = 2000,
+	lsp_file_methods = {
+		-- Time to wait for LSP file operations to complete before skipping
+		timeout_ms = 1000,
+		-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+		-- Set to "unmodified" to only save unmodified buffers
+		autosave_changes = false,
+	},
+	-- Constrain the cursor to the editable parts of the oil buffer
+	-- Set to `false` to disable, or "name" to keep it on the file names
+	constrain_cursor = "editable",
+	-- Set to true to watch the filesystem for changes and reload oil
+	experimental_watch_for_changes = false,
+	-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+	-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+	-- Additionally, if it is a string that matches "actions.<name>",
+	-- it will use the mapping at require("oil.actions").<name>
+	-- Set to `false` to remove a keymap
+	-- See :help oil-actions for a list of all available actions
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-s>"] = "actions.select_vsplit",
+		["<C-h>"] = "actions.select_split",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		["<C-l>"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["~"] = "actions.tcd",
+		["gs"] = "actions.change_sort",
+		["gx"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+		["g\\"] = "actions.toggle_trash",
+	},
+	-- Set to false to disable all of the above keymaps
+	use_default_keymaps = true,
+	view_options = {
+		-- Show files and directories that start with "."
+		show_hidden = true,
+		-- This function defines what is considered a "hidden" file
+		is_hidden_file = function(name, bufnr)
+			return vim.startswith(name, ".")
+		end,
+		-- This function defines what will never be shown, even when `show_hidden` is set
+		is_always_hidden = function(name, bufnr)
+			return false
+		end,
+		-- Sort file names in a more intuitive order for humans. Is less performant,
+		-- so you may want to set to false if you work with large directories.
+		natural_order = true,
+		sort = {
+			-- sort order can be "asc" or "desc"
+			-- see :help oil-columns to see which columns are sortable
+			{ "type", "asc" },
+			{ "name", "asc" },
+		},
+	},
+	-- Extra arguments to pass to SCP when moving/copying files over SSH
+	extra_scp_args = {},
+	-- EXPERIMENTAL support for performing file operations with git
+	git = {
+		-- Return true to automatically git add/mv/rm files
+		add = function(path)
+			return false
+		end,
+		mv = function(src_path, dest_path)
+			return false
+		end,
+		rm = function(path)
+			return false
+		end,
+	},
+	-- Configuration for the floating window in oil.open_float
+	float = {
+		-- Padding around the floating window
+		padding = 2,
+		max_width = 0,
+		max_height = 0,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		-- This is the config that will be passed to nvim_open_win.
+		-- Change values here to customize the layout
+		override = function(conf)
+			return conf
+		end,
+	},
+	-- Configuration for the actions floating preview window
+	preview = {
+		-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		-- min_width and max_width can be a single value or a list of mixed integer/float types.
+		-- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+		max_width = 0.9,
+		-- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+		min_width = { 40, 0.4 },
+		-- optionally define an integer/float for the exact width of the preview window
+		width = nil,
+		-- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		-- min_height and max_height can be a single value or a list of mixed integer/float types.
+		-- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+		max_height = 0.9,
+		-- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+		min_height = { 5, 0.1 },
+		-- optionally define an integer/float for the exact height of the preview window
+		height = nil,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		-- Whether the preview window is automatically updated when the cursor is moved
+		update_on_cursor_moved = true,
+	},
+	-- Configuration for the floating progress window
+	progress = {
+		max_width = 0.9,
+		min_width = { 40, 0.4 },
+		width = nil,
+		max_height = { 10, 0.9 },
+		min_height = { 5, 0.1 },
+		height = nil,
+		border = "rounded",
+		minimized_border = "none",
+		win_options = {
+			winblend = 0,
+		},
+	},
+	-- Configuration for the floating SSH window
+	ssh = {
+		border = "rounded",
+	},
+	-- Configuration for the floating keymaps help window
+	keymaps_help = {
+		border = "rounded",
+	},
+})
+
 
 
 local neogit = require('neogit')
 neogit.setup({})
 
-local lint = require("lint")
+-- local lint = require("lint")
 
-local conform = require("conform")
-conform.setup({})
+-- local conform = require("conform")
+-- conform.setup({})
+
+local treesitter_ctx = require("treesitter-context")
+treesitter_ctx.setup({
+	max_lines = 3,
+	separator = "-",
+	mode = "topline"
+})
 
 local map = vim.keymap.set
 
 map("n", "<leader>g", require("neogit").open)
+
 map("n", "<F12>", ":UndotreeToggle <Enter>")
-map("n", "<leader>vc", ":VimtexCompile <Enter>")
+
 map("n", "<leader>i", ":Inspect <Enter>")
+
+
 map("n", "<leader>ff", fzf.files)
 map("n", "<leader>fg", fzf.live_grep)
+
 map("n", "-", ":Oil<CR>")
 
+map("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { silent = true, noremap = true })
+map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { silent = true, noremap = true })
+map(
+	"n",
+	"<leader>xX",
+	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+	{ silent = true, noremap = true }
+)
 -- LSP
 --
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup("config", { clear = true })
 local function setup_lsp()
 	vim.lsp.enable({
-		"cssls", -- npm i -g vscode-langservers-extracted
-		"gopls", -- os package mgr: gopls
+		"cssls",
+		"eslint",
+		"gopls",
 		"html",
 		"jsonls",
-		"lua_ls", -- os package mgr: lua-language-server
-		"pyright", -- npm i -g pyright
-		"ts_ls", -- npm i -g typescript typescript-language-server
+		"lua_ls",
+		"pyright",
+		"shopify_theme_ls",
+		"svelte",
+		"tailwindcss",
+		"ts_ls",
+		"typos_lsp",
 		"html_lsp",
-		"tailwindcss"
+	})
+
+	vim.diagnostic.config({
+		virtual_text = false,
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.INFO] = "",
+				[vim.diagnostic.severity.HINT] = "",
+			},
+		},
+		update_in_insert = false,
+		underline = true,
+		severity_sort = true,
+		float = {
+			focusable = true,
+			style = "minimal",
+			border = "rounded",
+			source = true,
+			header = "",
+			prefix = "",
+		},
 	})
 
 	autocmd("LspAttach", {
@@ -184,6 +428,7 @@ local function setup_ts()
 			"javascript",
 			"json",
 			"lua",
+			"liquid",
 			"make",
 			"markdown",
 			"python",
@@ -225,4 +470,3 @@ end
 
 setup_ts()
 setup_lsp()
-require("treesitter-context").setup({ max_lines = 3, separator = ">", mode = "topline" })
