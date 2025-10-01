b253e9b69d1e476720f31602e883cbc7bdb1bcdc
kyle <kyle@kylelee.dev>
2025-10-01 12:23:55 -0400

    move autocmds to separate module

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,74 @@
+local group = "init"
+
+local autocmd= vim.api.nvim_create_autocmd
+
+vim.api.nvim_create_augroup(group, { clear = true })
+
 vim.cmd([[autocmd BufEnter * set formatoptions-=cro]])
 
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile", "BufWritePost" }, {
+
+autocmd("LspAttach", {
+	group = group,
+	callback = function(args)
+		local bufopts = { noremap = true, silent = true, buffer = args.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, bufopts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, bufopts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, bufopts)
+		vim.keymap.set("n", "M", function()
+			vim.diagnostic.open_float()
+		end, bufopts)
+		vim.keymap.set("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, bufopts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, bufopts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, bufopts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, bufopts)
+		--
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		local methods = vim.lsp.protocol.Methods
+
+		-- if client:supports_method(methods.textDocument_completion) then
+		-- 	vim.lsp.completion.enable(true, client.id, args.buf, {
+		-- 		autotrigger = true,
+		-- 		convert = function(item)
+		-- 			-- Don't preselect any item
+		-- 			item.preselect = false
+		-- 			return item
+		-- 		end
+		-- 	})
+		-- end
+
+		if
+			not client:supports_method("textDocument/willSaveWaitUntil")
+			and client:supports_method("textDocument/formatting")
+		then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup(group, { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					--disbale while I figured out if I want conform.nvim or LSP
+					-- vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
+})
+
+autocmd({ "BufReadPre", "BufNewFile", "BufWritePost" }, {
 	callback = function()
 		require("lint").try_lint()
 	end,
+	group=group
 })
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -40,64 +40,6 @@ return {
 			},
 		})
 
-		autocmd("LspAttach", {
-			group = augroup,
-			callback = function(args)
-				local bufopts = { noremap = true, silent = true, buffer = args.buf }
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, bufopts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, bufopts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
-				end, bufopts)
-				vim.keymap.set("n", "M", function()
-					vim.diagnostic.open_float()
-				end, bufopts)
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, bufopts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, bufopts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, bufopts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, bufopts)
-				--
-				local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-				local methods = vim.lsp.protocol.Methods
-
-				-- if client:supports_method(methods.textDocument_completion) then
-				-- 	vim.lsp.completion.enable(true, client.id, args.buf, {
-				-- 		autotrigger = true,
-				-- 		convert = function(item)
-				-- 			-- Don't preselect any item
-				-- 			item.preselect = false
-				-- 			return item
-				-- 		end
-				-- 	})
-				-- end
-
-				if
-					not client:supports_method("textDocument/willSaveWaitUntil")
-					and client:supports_method("textDocument/formatting")
-				then
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = vim.api.nvim_create_augroup("lsp", { clear = false }),
-						buffer = args.buf,
-						callback = function()
-							--disbale while I figured out if I want conform.nvim or LSP
-							-- vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-						end,
-					})
-				end
-			end,
-		})
 	end,
 	defer = true,
 	dependencies = {
