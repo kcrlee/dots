4ab0ad979f9995ac9c149f2c18a2867b259f2435
kyle <kyle@kylelee.dev>
2025-10-06 13:46:21 -0400

    add wezterm-types

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -4,9 +4,14 @@ return {
 		lazydev.setup({
 			library = {
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "wezterm-types", mods = { "wezterm" } },
 			},
 		})
 	end,
+	dependencies = {
+		src = "https://github.com/DrKJeff16/wezterm-types",
+		defer = true,
+	},
 	defer = true,
 	src = "https://github.com/folke/lazydev.nvim",
 }
