07592ffa3949ece4e4b022488ecff3da6e03015c
Kyle Lee <kyle@kylelee.dev>
2025-10-23 21:28:05 -0400

    add noice macro recording message

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,18 @@
 return {
 	config = function()
 		local lualine = require("lualine")
-		lualine.setup({})
+		lualine.setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					}
+				},
+			},
+
+		})
 	end,
 	defer = true,
 	src = "https://github.com/nvim-lualine/lualine.nvim",
