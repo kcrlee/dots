354db63df65ba248c6c36347b4f2c418609b57a1
Kyle Lee <kyle@kylelee.dev>
2025-11-06 19:54:01 -0500

    add blink back

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,10 +7,6 @@ return {
 			defer = true,
 			src = "https://github.com/rktjmp/lush.nvim",
 		},
-		{
-			defer = true,
-			src = "https://github.com/nvim-lua/plenary.nvim",
-		},
 	},
 	defer = true,
 	src = "https://github.com/kcrlee/tomorrow-min",
