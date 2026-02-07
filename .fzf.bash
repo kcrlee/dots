81f3fcd70c005a62bfc3240ddd6cd3432b1908c5
Kyle Lee <kyle@kylelee.dev>
2026-02-06 21:26:25 -0500

    add fzf.bash and .fdignore

--- .fzf.bash
+++ .fzf.bash
@@ -1,12 +1,12 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+	PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2>/dev/null
 
 # Key bindings
 # ------------
