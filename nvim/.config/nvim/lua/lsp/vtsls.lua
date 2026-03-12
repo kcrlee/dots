61579c615fd0089caa22e4a0634f1a864b67010f
kyle <kyle@kylelee.dev>
2026-03-12 18:43:17 -0400

    update ghostty

--- nvim/.config/nvim/lua/lsp/vtsls.lua
+++ nvim/.config/nvim/lua/lsp/vtsls.lua
@@ -67,30 +67,30 @@
 
 ---@type vim.lsp.Config
 return {
-	cmd = { 'vtsls', '--stdio' },
+	cmd = { "vtsls", "--stdio" },
 	init_options = {
-		hostInfo = 'neovim',
+		hostInfo = "neovim",
 	},
 	filetypes = {
-		'javascript',
-		'javascriptreact',
-		'javascript.jsx',
-		'typescript',
-		'typescriptreact',
-		'typescript.tsx',
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
 	},
 	root_dir = function(bufnr, on_dir)
 		-- The project root is where the LSP can be started from
 		-- As stated in the documentation above, this LSP supports monorepos and simple projects.
 		-- We select then from the project root, which is identified by the presence of a package
 		-- manager lock file.
-		local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+		local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
 		-- Give the root markers equal priority by wrapping them in a table
-		root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
-			or vim.list_extend(root_markers, { '.git' })
+		root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
+			or vim.list_extend(root_markers, { ".git" })
 
 		-- exclude deno
-		if vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc', 'deno.lock' }) then
+		if vim.fs.root(bufnr, { "deno.json", "deno.jsonc", "deno.lock" }) then
 			return
 		end
 
