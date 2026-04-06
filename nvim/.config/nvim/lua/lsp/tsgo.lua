6214c5d702d10b648d88085972b6906b0d89b6cb
kyle <kyle@kylelee.dev>
2026-04-06 13:03:12 -0400

    format LSP file

--- nvim/.config/nvim/lua/lsp/tsgo.lua
+++ nvim/.config/nvim/lua/lsp/tsgo.lua
@@ -50,7 +50,7 @@ return {
 		typescript = {
 			inlayHints = {
 				parameterNames = {
-					enabled = 'literals',
+					enabled = "literals",
 					suppressWhenArgumentMatchesName = true,
 				},
 				parameterTypes = { enabled = true },
@@ -62,31 +62,31 @@ return {
 		},
 	},
 	cmd = function(dispatchers, config)
-		local cmd = 'tsgo'
-		local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/tsgo'
+		local cmd = "tsgo"
+		local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/tsgo"
 		if local_cmd and vim.fn.executable(local_cmd) == 1 then
 			cmd = local_cmd
 		end
-		return vim.lsp.rpc.start({ cmd, '--lsp', '--stdio' }, dispatchers)
+		return vim.lsp.rpc.start({ cmd, "--lsp", "--stdio" }, dispatchers)
 	end,
 	filetypes = {
-		'javascript',
-		'javascriptreact',
-		'typescript',
-		'typescriptreact',
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
 	},
 	root_dir = function(bufnr, on_dir)
 		-- The project root is where the LSP can be started from
 		-- As stated in the documentation above, this LSP supports monorepos and simple projects.
 		-- We select then from the project root, which is identified by the presence of a package
 		-- manager lock file.
-		local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+		local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
 		-- Give the root markers equal priority by wrapping them in a table
-		root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
-			or vim.list_extend(root_markers, { '.git' })
+		root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
+			or vim.list_extend(root_markers, { ".git" })
 
-		local deno_root = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' })
-		local deno_lock_root = vim.fs.root(bufnr, { 'deno.lock' })
+		local deno_root = vim.fs.root(bufnr, { "deno.json", "deno.jsonc" })
+		local deno_lock_root = vim.fs.root(bufnr, { "deno.lock" })
 		local project_root = vim.fs.root(bufnr, root_markers)
 		if deno_lock_root and (not project_root or #deno_lock_root > #project_root) then
 			-- deno lock is closer than package manager lock, abort
