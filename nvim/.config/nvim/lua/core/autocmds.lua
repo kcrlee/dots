14647311867e5e73c85e22fd4c1440f69fbb4e9c
Kyle Lee <kyle@kylelee.dev>
2026-04-28 21:19:45 -0400

    remove noice

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -34,7 +34,11 @@ autocmd("LspAttach", {
 			vim.lsp.buf.definition()
 		end, bufopts)
 		vim.keymap.set("n", "K", function()
-			vim.lsp.buf.hover({ anchor_bias = 'auto', zindex = 300 })
+			vim.lsp.buf.hover({
+				buffer = "rounded",
+				anchor_bias = 'auto',
+				zindex = 300
+			})
 		end, bufopts)
 		vim.keymap.set("n", "<leader>vws", function()
 			vim.lsp.buf.workspace_symbol()
