90f9c5921fc62f79b5beac5ac6601026f5ea0029
Kyle Lee <kyle@kylelee.dev>
2026-09-04 22:30:25 -0400

    fix blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -14,15 +14,15 @@ blink.setup({
 			-- Whether to enable the frecency feature
 			enabled = true,
 			-- Location of the frecency database
-			path = vim.fn.stdpath('state') .. '/blink/cmp/frecency.dat',
+			path = vim.fn.stdpath("state") .. "/blink/cmp/frecency.dat",
 			-- UNSAFE!! When enabled, disables the lock and fsync when writing to the frecency database.
 			-- This should only be used on unsupported platforms (e.g. alpine, termux)
-			unsafe_no_lock = false
+			unsafe_no_lock = false,
 		},
 		prebuilt_binaries = {
 			download = true,
-			force_version = "1.*"
-		}
+			force_version = "1.*",
+		},
 	},
 	signature = {
 		enabled = true,
@@ -33,43 +33,43 @@ blink.setup({
 			max_height = 80,
 			scrollbar = false,
 			treesitter_highlighting = true,
-			show_documentation = true
-		}
+			show_documentation = true,
+		},
 	},
 	snippets = { preset = "default" },
 	sources = {
 		default = { "lsp", "lazydev", "path", "snippets", "buffer" },
 		per_filetype = {
-			lua = { inherit_defaults = true, "lazydev" }
+			lua = { inherit_defaults = true, "lazydev" },
 		},
 		providers = {
 			lsp = {
 				name = "lsp",
 				enabled = true,
 				module = "blink.cmp.sources.lsp",
-				max_items = 50
+				max_items = 50,
 			},
 			lazydev = {
 				name = "LazyDev",
 				module = "lazydev.integrations.blink",
-				score_offset = 100
-			}
-		}
+				score_offset = 100,
+			},
+		},
 	},
 	cmdline = {
-		enabled = true
+		enabled = true,
 	},
 	completion = {
 		list = {
 			selection = {
 				preselect = false,
-				auto_insert = false
-			}
+				auto_insert = false,
+			},
 		},
 		accept = {
 			auto_brackets = {
-				enabled = false
-			}
+				enabled = false,
+			},
 		},
 		menu = {
 			border = "rounded",
@@ -80,13 +80,13 @@ blink.setup({
 				components = {
 					source_name = {
 						width = { max = 30 },
-						text = function (ctx)
+						text = function(ctx)
 							return "[" .. ctx.source_name .. "]"
 						end,
-						highlight = "BlinkCmpSource"
-					}
-				}
-			}
+						highlight = "BlinkCmpSource",
+					},
+				},
+			},
 		},
 		documentation = {
 			auto_show = true,
@@ -97,15 +97,15 @@ blink.setup({
 				max_width = 60,
 				max_height = 20,
 				direction_priority = {
-					menu_north = { 'e', 'w', 's', 'n' },
-					menu_south = { "e", "w" }
-				}
-			}
-		}
+					menu_north = { "e", "w", "s", "n" },
+					menu_south = { "e", "w" },
+				},
+			},
+		},
 	},
 	appearance = {
 		use_nvim_cmp_as_default = false,
-		nerd_font_variant = "mono"
+		nerd_font_variant = "mono",
 	},
 	keymap = {
 		["<C-space>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
@@ -116,6 +116,6 @@ blink.setup({
 		["<C-b>"] = { "scroll_documentation_down", "fallback" },
 		["<C-f>"] = { "scroll_documentation_up", "fallback" },
 		["<C-l>"] = { "snippet_forward", "fallback" },
-		["<C-h>"] = { "snippet_backward", "fallback" }
-	}
+		["<C-h>"] = { "snippet_backward", "fallback" },
+	},
 })
