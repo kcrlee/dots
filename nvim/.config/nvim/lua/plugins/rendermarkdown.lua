e14297435778878a4d487a984fcc81f7437dfa4a
Kyle Lee <kyle@kylelee.dev>
2026-09-19 00:53:17 -0400

    add codediff config

--- nvim/.config/nvim/lua/plugins/rendermarkdown.lua
+++ nvim/.config/nvim/lua/plugins/rendermarkdown.lua
@@ -3,7 +3,7 @@ vim.pack.add({ "https://github.com/MeanderingProgrammer/render-markdown.nvim" })
 require("render-markdown").setup({
 	code = {
 		style = "language",
-		border = "thin",
+		border = "thick",
 		highlight = "Normal",
 		highlight_inline = "Normal",
 	},
