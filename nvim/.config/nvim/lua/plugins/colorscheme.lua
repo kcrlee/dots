9f618aa528cae76d57f7da905daa98d8bfa7f929
kyle <kyle@kylelee.dev>
2026-09-22 11:30:41 -0400

    use luna

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,17 +9,17 @@ vim.pack.add({
 	"https://github.com/WTFox/luna.nvim",
 })
 
-vim.cmd([[colorscheme tomorrow-min]])
+vim.cmd([[colorscheme luna]])
 
-local function flatten_float_hls()
-	vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
-	vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-	vim.api.nvim_set_hl(0, "@markup.raw.block.markdown", { bg = "NONE" })
-	vim.api.nvim_set_hl(0, "@markup.raw.delimiter.markdown", { bg = "NONE" })
-	vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { bg = "NONE" })
-end
-vim.api.nvim_create_autocmd("ColorScheme", {
-	group = vim.api.nvim_create_augroup("my.float.bg", { clear = true }),
-	callback = flatten_float_hls,
-})
-flatten_float_hls()
+-- local function flatten_float_hls()
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+-- 	vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+-- 	vim.api.nvim_set_hl(0, "@markup.raw.block.markdown", { bg = "NONE" })
+-- 	vim.api.nvim_set_hl(0, "@markup.raw.delimiter.markdown", { bg = "NONE" })
+-- 	vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { bg = "NONE" })
+-- end
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+-- 	group = vim.api.nvim_create_augroup("my.float.bg", { clear = true }),
+-- 	callback = flatten_float_hls,
+-- })
+-- flatten_float_hls()
